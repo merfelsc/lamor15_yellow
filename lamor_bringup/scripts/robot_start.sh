13f7d2aa3dadadabec89e413041a7164b3c4d90f
Ali <ali_abdulkhaliq@yahoo.com>
2015-08-30 16:00:20 +0200

    all launch files merged in the robot_start script

--- lamor_bringup/scripts/robot_start.sh
+++ lamor_bringup/scripts/robot_start.sh
@@ -11,8 +11,15 @@ tmux new-window -t $SESSION:3 -n 'cameras'
 tmux new-window -t $SESSION:4 -n 'ui'
 tmux new-window -t $SESSION:5 -n 'navigation'
 tmux new-window -t $SESSION:6 -n 'ppl_perception'
-tmux new-window -t $SESSION:7 -n 'scheduler'
-tmux new-window -t $SESSION:8 -n 'control'
+#tmux new-window -t $SESSION:7 -n 'scheduler'
+#tmux new-window -t $SESSION:8 -n 'control'
+#New windows:
+tmux new-window -t $SESSION:7 -n 'mary'
+tmux new-window -t $SESSION:8 -n 'facts'
+tmux new-window -t $SESSION:9 -n 'weather'
+tmux new-window -t $SESSION:10 -n 'random_walk' 
+tmux new-window -t $SESSION:11 -n 'controller'
+
 
 
 tmux select-window -t $SESSION:0
@@ -40,6 +47,22 @@ tmux send-keys "DISPLAY=:0 roslaunch lamor_bringup lamor_navigation.launch chest
 
 tmux select-window -t $SESSION:6
 tmux send-keys "DISPLAY=:0 roslaunch perception_people_launch people_tracker_robot.launch machine:=$HEAD_PC user:=lamor"
+
+tmux select-window -t $SESSION:7
+tmux send-keys "DISPLAY=:0 roslaunch mary_tts ros_mary.launch"
+
+tmux select-window -t $SESSION:8
+tmux send-keys "DISPLAY=:0 rosrun facts tellFacts.py"
+
+tmux select-window -t $SESSION:9
+tmux send-keys "DISPLAY=:0 rosrun weather tell_weather.py"
+
+tmux select-window -t $SESSION:10
+tmux send-keys "DISPLAY=:0 rosrun random_walker random_walker.py"
+
+tmux select-window -t $SESSION:11
+tmux send-keys "DISPLAY=:0 roslaunch controller controller_robot.launch"
+
 
 # Set default window
 tmux select-window -t $SESSION:0
