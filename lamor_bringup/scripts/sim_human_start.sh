5f6274fb6fd7b4a78cf14b4027fc7e6ceb876599
brutus <brutusthetschiepel@gmail.com>
2016-02-17 14:06:24 +0000

    Merge remote-tracking branch 'strands-project/master'
    # Conflicts:
    #	lamor_bringup/scripts/sim_start.sh

--- lamor_bringup/scripts/sim_human_start.sh
+++ lamor_bringup/scripts/sim_human_start.sh
@@ -9,6 +9,8 @@ tmux new-window -t $SESSION:1 -n 'core'
 tmux new-window -t $SESSION:2 -n 'simulation'
 tmux new-window -t $SESSION:3 -n '2d_nav'
 tmux new-window -t $SESSION:4 -n 'topo_nav'
+tmux new-window -t $SESSION:5 -n 'ppl_emulator'
+tmux new-window -t $SESSION:6 -n 'qsr_lib'
 
 
 tmux select-window -t $SESSION:0
@@ -23,13 +25,19 @@ tmux select-window -t $SESSION:1
 tmux send-keys "DISPLAY=:0 roslaunch mongodb_store mongodb_store.launch db_path:=$HOME/mongodb_lamor"
 
 tmux select-window -t $SESSION:2
-tmux send-keys "DISPLAY=:0 roslaunch strands_morse uol_bl_morse.launch env:=uol_bl_fast"
+tmux send-keys "DISPLAY=:0 roslaunch strands_morse uol_bl_morse.launch env:=uol_bl_human_fast"
 
 tmux select-window -t $SESSION:3
 tmux send-keys "DISPLAY=:0 roslaunch lamor_bringup uol_bl_nav2d.launch map:=\$(rospack find strands_morse)/uol/maps/uol_bl.yaml"
 
 tmux select-window -t $SESSION:4
 tmux send-keys "DISPLAY=:0 roslaunch lamor_bringup lamor_sim_navigation.launch dataset:=bl_sim"
+
+tmux select-window -t $SESSION:5
+tmux send-keys "DISPLAY=:0 rosrun people_tracker_emulator posestamped_to_ppl_tracker_msgs.py"
+
+tmux select-window -t $SESSION:6
+tmux send-keys "DISPLAY=:0 rosrun qsr_lib qsrlib_ros_server.py"
 
 
 # Set default window
